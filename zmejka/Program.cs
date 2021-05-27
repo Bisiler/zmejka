8fe16de65fc3bf348e8063521ecda6b868a04a88
opilane <opilane@HP-CZC6297BBL>
2021-05-27 12:51:09 +0300

    lessson 3 class point создан

--- zmejka/Program.cs
+++ zmejka/Program.cs
@@ -6,29 +6,33 @@ namespace zmejka
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
 
-            int x2 = 4;
+            /*int x1 = 1;
+            int y1 = 3;//тут устанавливаем координаты для знанка *
+            char sym1 = '*';
+            Console.SetCursorPosition(x1, y1);//тут мы даем курсору позицию по координатам которые мы ввели выше
+            Console.Write(sym1);//тут мы выводим на экран символ в данном случяи это * */
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            p2.Draw();
+            /*int x2 = 4;//тут устанавливаем координаты для знанка #
             int y2 = 5;
             char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
-
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
+            Draw(x2, y2, sym2)
+            Console.SetCursorPosition(x2, y2);//тут мы даем курсору позицию по координатам которые мы ввели выше
+            Console.Write(sym2);//тут мы выводим на экран символ в данном случяи это #*/
 
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-             Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-
     }
 }
